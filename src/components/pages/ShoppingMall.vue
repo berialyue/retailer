5596cfa8d588a688c87cbeaaf4ee902097f73b48
berialyue <berialyue@gmail.com>
2018-07-26 23:06:10 +0800

    add swiper

--- src/components/pages/ShoppingMall.vue
+++ src/components/pages/ShoppingMall.vue
@@ -13,6 +13,13 @@
         </van-col>
       </van-row>
     </div>
+    <div class="swiper-area">
+      <van-swipe :autoplay='3000'>
+        <van-swipe-item v-for='(banner,index) in bannerPicArray' :key='index'>
+          <img :src="banner.imageUrl" class="swipe-img">
+        </van-swipe-item>
+      </van-swipe>
+    </div>
   </div>
 </template>
 
@@ -21,36 +28,43 @@ export default {
   name: 'ShoppingMall',
   data () {
     return {
-      locationIcon: require('../../assets/images/location.png')
+      locationIcon: require('../../assets/images/location.png'),
+      bannerPicArray: [
+        {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic001.jpg'},
+        {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic002.jpg'},
+        {imageUrl: 'http://7xjyw1.com1.z0.glb.clouddn.com/simleVueDemoPic003.jpg'}
+      ]
     }
   }
 }
 </script>
 
-<style scoped>
-.search-bar{
-  height: 2.2rem;
-  background-color: #e5017d;
-  line-height: 2.2rem;
-}
-.location-icon{
-  width: 60%;
-  padding-top: .4rem;
-  padding-left: .5rem;
-}
-.search-input{
-  width: 100%;
-  height: 1.3rem;
-  border-top: 0px;
-  border-left: 0px;
-  border-right: 0px;
-  border-bottom: 1px solid #ffffff !important;
-  background-color: #e5017d;
+<style lang="stylus" scoped>
+.search-bar
+  height: 2.2rem
+  background-color: #e5017d
+  line-height: 2.2rem
+.location-icon
+  width: 60%
+  padding-top: .4rem
+  padding-left: .5rem
+.search-input
+  width: 100%
+  height: 1.3rem
+  border-top: 0px
+  border-left: 0px
+  border-right: 0px
+  border-bottom: 1px solid #ffffff !important
+  background-color: #e5017d
   color: #ffffff
-}
-.search-btn{
-  margin-left: .5rem;
-  background-color: #cccccc;
+.search-btn
+  margin-left: .5rem
+  background-color: #cccccc
   border: 0px
-}
+.swiper-area
+  width: 20rem
+  overflow: hidden
+  clear: both
+.swipe-img
+  width: 100%
 </style>
