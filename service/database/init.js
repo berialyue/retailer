3e3b994f2bc14770165c55057b7fd5cc8ef1f6ba
berialyue <berialyue@gmail.com>
2018-08-09 22:24:54 +0800

    add mongodb error message

--- service/database/init.js
+++ service/database/init.js
@@ -7,20 +7,37 @@ exports.connect = () => {
   // 连接数据库
   mongoose.connect(db)
 
-  // 增加数据库连接的事件监听
-  mongoose.connection.on('disconnected', () => {
-    // 进行重连
-    mongoose.connect(db)
-  })
+  let maxConnectTimes = 0
 
-  // 数据库出现错误的时候
-  mongoose.connection.on('error', err => {
-    console.log(err)
-    mongoose.connect(db)
-  })
+  return new Promise((resolve, reject) => {
+    // 增加数据库连接的事件监听
+    mongoose.connection.on('disconnected', () => {
+      // 进行重连
+      console.log('**********************数据库断开**********************')
+      if (maxConnectTimes < 3) {
+        maxConnectTimes++
+        mongoose.connect(db)
+      } else {
+        reject(new Error('数据库出现问题，请手动修复'))
+      }
+    })
 
-  // 链接打开的时候
-  mongoose.connection.once('open', () => {
-    console.log('MongoDB Connected successfully!')
+    // 数据库出现错误的时候
+    mongoose.connection.on('error', err => {
+      console.log('**********************数据库断开**********************')
+      if (maxConnectTimes < 3) {
+        maxConnectTimes++
+        mongoose.connect(db)
+      } else {
+        reject(err)
+        throw new Error('数据库出现问题，请手动修复')
+      }
+    })
+
+    // 链接打开的时候
+    mongoose.connection.once('open', () => {
+      console.log('MongoDB Connected successfully!')
+      resolve()
+    })
   })
 }
