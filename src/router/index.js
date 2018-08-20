c67991c9824c49785d4d6911c48551353d5b28ac
berialyue <berialyue@gmail.com>
2018-08-20 23:09:38 +0800

    add register

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import ShoppingMall from '@/components/pages/ShoppingMall'
-
+import Register from '@/components/pages/Register'
 Vue.use(Router)
 
 export default new Router({
@@ -10,6 +10,10 @@ export default new Router({
       path: '/',
       name: 'ShoppingMall',
       component: ShoppingMall
+    }, {
+      path: '/register',
+      name: 'Register',
+      component: Register
     }
   ]
 })
