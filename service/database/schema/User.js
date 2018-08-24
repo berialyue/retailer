f399d733f4d8b1d3ae1e3d9fa25854fcc09f8172
berialyue <berialyue@gmail.com>
2018-08-24 21:33:41 +0800

    add reg

--- service/database/schema/User.js
+++ service/database/schema/User.js
@@ -1,19 +1,19 @@
 const mongoose = require('mongoose')
-const bcrypt = require('bcrypt')
 const Schema = mongoose.Schema
 let ObjectId = Schema.Types.ObjectId
+const bcrypt = require('bcrypt')
 const SALT_WORK_FACTOR = 10
 
-// 创建我们的用户Schema
+// 创建UserShema
 const userSchema = new Schema({
-  userId: ObjectId,
+  UserId: {type: ObjectId},
   userName: {unique: true, type: String},
   password: String,
   createAt: {type: Date, default: Date.now()},
   lastLoginAt: {type: Date, default: Date.now()}
+}, {
+  collection: 'user'
 })
-
-// 每次存储数据时都要执行
 userSchema.pre('save', function (next) {
   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
     if (err) return next(err)
@@ -24,15 +24,17 @@ userSchema.pre('save', function (next) {
     })
   })
 })
-// userSchema.pre('save', function (next) {
-//   bcrypt.genSalt(SALT_WORK_FACTOR, (err, salt) => {
-//     if (err) return next(err)
-//     bcrypt.hash(this.password, salt, (err, hash) => {
-//       if (err) return next(err)
-//       this.password = hash
-//       next()
-//     })
-//   })
-// })
+
+userSchema.methods = {
+  comparePassword: (_password, password) => {
+    return new Promise((resolve, reject) => {
+      bcrypt.compare(_password, password, (err, isMatch) => {
+        if (!err) resolve(isMatch)
+        else reject(err)
+      })
+    })
+  }
+}
+
 // 发布模型
 mongoose.model('User', userSchema)
