45d08d14b9893a80323592e6a575f3ebaedcc9e9
berialyue <berialyue@gmail.com>
2018-08-27 22:15:02 +0800

    add localStorage.userinfo

--- service/index.js
+++ service/index.js
@@ -1,18 +1,53 @@
+// const Koa = require('koa')
+// const app = new Koa()
+// // const mongoose = require('mongoose')
+// const { connect, initSchemas } = require('./database/init.js')
+// const Router = require('koa-router')
+// const bodyParser = require('koa-bodyparser')
+// const cors = require('koa2-cors')
+
+// app.use(cors())
+// app.use(bodyParser())
+
+// let user = require('./appApi/User.js')
+
+// let router = new Router()
+// router.use('/user', user.routes())
+
+// app.use(router.routes())
+// app.use(router.allowedMethods())
+
+// ;(async () => {
+//   await connect()
+//   initSchemas()
+// })()
+
+// app.listen(3000, () => {
+//   console.log('[Server] starting at port 3000')
+// })
 const Koa = require('koa')
 const app = new Koa()
-const mongoose = require('mongoose')
 const { connect, initSchemas } = require('./database/init.js')
-const Router = require('koa-router')
+const mongoose = require('mongoose')
 const bodyParser = require('koa-bodyparser')
 const cors = require('koa2-cors')
+const Router = require('koa-router')
 
-app.use(cors())
 app.use(bodyParser())
+app.use(cors())
 
-let user = require('./appApi/User')
+let user = require('./appApi/user.js')
+// let home = require('./appApi/home.js')
+// let goods = require('./appApi/goods.js')
 
+// 装载所有子路由
 let router = new Router()
 router.use('/user', user.routes())
+// router.use('/home', home.routes())
+// router.use('/goods', goods.routes())
+// egg.js
+
+// 加载路由中间件
 
 app.use(router.routes())
 app.use(router.allowedMethods())
@@ -23,7 +58,7 @@ app.use(router.allowedMethods())
 })()
 
 app.use(async (ctx) => {
-  ctx.body = '</h1>hello world</h1>'
+  ctx.body = '<h1>Hello Koa2</h1>'
 })
 
 app.listen(3000, () => {
--- src/serviceAPI.config.js
+++ src/serviceAPI.config.js
@@ -3,7 +3,8 @@ const LOCALURL = 'http://localhost:3000/'
 const URL = {
   getShoppingMallInfo: BASEURL + 'index',
   getGoodsInfo: BASEURL + 'getGoodsInfo',
-  registerUser: LOCALURL + 'user/register'
+  registerUser: LOCALURL + 'user/register',
+  login: LOCALURL + 'user/login'
 }
 
 module.exports = URL
